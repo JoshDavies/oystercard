32300d31b20e80f949b7872c3b7b41da259120c5
Student <student@Makerss-MacBook-Pro.local>
2019-09-23 15:44:09 +0100

    set up tests for card balance and adding an amount

---
+++ lib/Oystercard.rb
@@ -0,0 +1,6 @@
+class Oystercard
+DEFAULT_BALANCE = 0
+  def add_money(amount)
+
+  end
+end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,1 +1,36 @@
 require 'Oystercard'
+
+describe Oystercard do
+  it { is_expected.to respond_to(:add_money).with(1).argument }
+
+  describe 'initialization' do
+    subject { Oystercard.new }
+
+    it 'creates a card with default balance' do
+      expect(subject.balance).to eq described_class::DEFAULT_BALANCE
+    end
+  end
+
+  describe '#add_money' do
+    it 'adds an amount to a customers Oystercard' do
+      card = Oystercard.new
+      expect(card.add_money(0)).to eq 0
+    end
+  end
+
+end
+
+=begin
+  In order to use public transport
+  As a customer
+  I want money on my card
+
+  objects
+  card
+  customer
+
+  -messages
+  add_money
+
+  card --> add_money --> balance
+=end
